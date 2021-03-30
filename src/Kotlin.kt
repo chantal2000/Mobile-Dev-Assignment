1cf35d41ce50df8b593e87e3be5a7ac56a2e9cbc
chantal2000 <nniyonkuruchantal@gmail.com>
2021-03-30 20:30:52 +0300

    Kotlin Assignment

---
+++ src/Kotlin.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var student="Niyonkuru Marie Chantal"
+    println(student)
+    var age=30
+    println(age)
+    var PNumber="0786738069"
+    println(PNumber)
+    var weight="56kgs"
+    println(weight)
+    var citizen="Rwanda"
+    var kenyanCitizen:Boolean=false
+    println(kenyanCitizen)
+}
